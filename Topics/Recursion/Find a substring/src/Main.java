8c432e9c14cff28894280cdfdb8c944229748da4
David <27825010+David-U876@users.noreply.github.com>
2025-01-02 12:12:08 -0600

    Delete Topics directory

--- Topics/Recursion/Find a substring/src/Main.java
+++
@@ -1,9 +0,0 @@
-class Util {
-    public static int indexOf(String src, String tgt) {
-        // your code here
-        if (src.length() > tgt.length()) {
-            return src.indexOf(tgt);
-        }
-        return -1;
-    }
-}
